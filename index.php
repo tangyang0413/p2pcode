c1ed49ef20146f720ad3791ba0e155b16051405a
tangyang <1013612645@qq.com>
2018-08-05 16:09:41 +0800

    2018/8/5 项目公共抽取完成，模板完成 项目开始分配

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 		<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css"/>
 		<!--引入自定义样式-->
 		<link rel="stylesheet" type="text/css" href="dist/css/min/index.min.css"/>
+		<!--引入图标字体-->
+		<link rel="stylesheet" type="text/css" href="lib/font-awesome/css/font-awesome.min.css"/>
 		<!--兼容低版本的浏览器-->
 		<!--[if lt IE 9]>
 	      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -19,77 +21,14 @@
 
 	<body>
 		<!--页头-->
-		<!--副导航-->
-		<nav class="navbar navbar-inverse">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbox01" aria-expanded="false">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				    </button>
-					<a class="navbar-brand" href="#"><span class="glyphicon glyphicon-phone-alt"></span>028-86261949</a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="navbox01">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">首页</a></li>
-						<li><a href="#">小强</a></li>
-						<li><a href="#">赶快充值</a></li>
-						<li><a href="#">注销</a></li>
-						<li><a href="#">帮助</a></li>
-						<li><a href="#">联系客服</a></li>
-					</ul>
-				</div>
-				<!-- /.navbar-collapse -->
-			</div>
-			<!-- /.container-fluid -->
-		</nav>
-		
-		
-		
-		
-		
-		<!--主导航-->
-		<nav class="navbar navbar-default">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				    </button>
-					<a class="navbar-brand" href="#"><img src="static/images/logo.png"/></a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
-						<li><a href="#">我要投资</a></li>
-						<li><a href="#">我要借款</a></li>
-						<li><a href="#">个人中心</a></li>
-						<li><a href="#">新手指引</a></li>
-						<li><a href="#">关于我们</a></li>
-					</ul>
-				</div>
-				<!-- /.navbar-collapse -->
-			</div>
-			<!-- /.container-fluid -->
-		</nav>
-		
-		
-		
+		<?php
+			include_once("./header.html");	
+		?>
 		
 		
 		
 		<!--轮播图-->
-		<div class="container" id="carousel">
+		<div id="carousel">
 			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 			  <!-- Indicators -->
 			  <ol class="carousel-indicators">
@@ -163,10 +102,10 @@
 			      <thead>
 			        <tr>
 			          <th>借款人</th>
-			          <th>借款标题</th>
+			          <th class="width620">借款标题</th>
 			          <th>年利率</th>
 			          <th>金额</th>
-			          <th>还款方式</th>
+			          <th class="width620">还款方式</th>
 			          <th>进度</th>
 			          <th>操作</th>
 			        </tr>
@@ -174,28 +113,28 @@
 			      <tbody>
 			        <tr>
 			          <th scope="row">张三</th>
-			          <td>给我2000度过难关</td>
+			          <td class="width620">给我2000度过难关</td>
 			          <td>10.00%</td>
 			          <td>2,000.00</td>
-			          <td>按月分期还款</td>
+			          <td class="width620">按月分期还款</td>
 			          <td>78.00%</td>
 			          <td><button type="button" class="btn btn-sm btn-danger">查看</button></td>
 			        </tr>
 			        <tr>
 			          <th scope="row">张三</th>
-			          <td>给我2000度过难关</td>
+			          <td class="width620">给我2000度过难关</td>
 			          <td>10.00%</td>
 			          <td>2,000.00</td>
-			          <td>按月分期还款</td>
+			          <td class="width620">按月分期还款</td>
 			          <td>78.00%</td>
 			          <td><button type="button" class="btn btn-sm btn-danger">查看</button></td>
 			        </tr>
 			        <tr>
 			          <th scope="row">张三</th>
-			          <td>给我2000度过难关</td>
+			          <td class="width620">给我2000度过难关</td>
 			          <td>10.00%</td>
 			          <td>2,000.00</td>
-			          <td>按月分期还款</td>
+			          <td class="width620">按月分期还款</td>
 			          <td>78.00%</td>
 			          <td><button type="button" class="btn btn-sm btn-danger">查看</button></td>
 			        </tr>
@@ -223,10 +162,10 @@
 					  </div>
 					  <div class="panel-body">
 					     <ul>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
 					     </ul>
 					  </div>
 					</div>
@@ -241,10 +180,10 @@
 					  </div>
 					  <div class="panel-body">
 					     <ul>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
 					     </ul>
 					  </div>
 					</div>
@@ -259,10 +198,10 @@
 					  </div>
 					  <div class="panel-body">
 					     <ul>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
 					     </ul>
 					  </div>
 					</div>
@@ -277,10 +216,10 @@
 					  </div>
 					  <div class="panel-body">
 					     <ul>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
-					     	<li>我是隔壁的泰山 <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
+					     	<li><a href="#">我是隔壁的泰山</a> <span>发表日期：2018-08-04</span></li>
 					     </ul>
 					  </div>
 					</div>
@@ -291,33 +230,9 @@
 		
 		
 		<!--页脚-->
-		<div class="row el-footer">
-		    <div class="container">
-		        <div class="col-sm-8">
-		            <p>专业Java培训、PHP培训、UI培训、iOS培训、高级Web前端培训机构</p>
-		            <p>版权所有： 2006-2015成都源代码教育咨询有限公司</p>
-		            <p>地  址： 成都市高新区府城大道西段399号天府新谷1号楼620-625</p>
-		            <p>电  话： 028-86261949&nbsp; / 400-808-6840 邮箱： yuandaima@itsource.cn</p>
-		            <p>
-		                <a href="http://www.miitbeian.gov.cn">蜀ICP备14030149号-1</a>  <a href="http://www.gaj.chengdu.gov.cn">蜀公网安备510114990541</a>
-		            </p>
-		            <div style="display: inline-block; overflow: hidden; float: left;"></div>
-		        </div>
-		        <div class="col-sm-2">
-		            <div>
-		                <p style="padding-bottom: 15px; font-size: 14px; padding-left: 10px;">官方微博：</p>
-		                <a href="http://t.qq.com/it-source" target="_blank"> <img src="static/images/tecent.png" alt="Responsive image"></a>
-		                <a href="http://weibo.com/cditcast" target="_blank"> <img src="static/images/sina.png" alt="Responsive image"></a>
-		            </div>
-		        </div>
-		        <div class="col-sm-2">
-		            <div>
-		                <p style="padding-bottom: 15px; font-size: 14px; padding-left: 10px;">官方微信：</p>
-		                <img src="static/images/wx.jpg" alt="Responsive image" class="img-responsive">
-		            </div>
-		        </div>
-		    </div>
-		</div>
+		<?php
+			include_once("./footer.html");	
+		?>
 		
 		
 	</body>
@@ -325,5 +240,6 @@
 	<script src="lib/jquery/jquery-1.11.0.js"></script>
 	<!--引入bootstrap  js文件-->
 	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
-	
+	<!--引入提示框的js文件-->
+	<script src="src/javascript/titlebox.js"></script>
 </html>
---
+++ template.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<title>p2p金融借贷平台————首页</title>
+		<!--视口: 兼容移动端-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+		<!--引入bootstarp样式-->
+		<link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.min.css"/>
+		<!--引入自定义样式-->
+		<link rel="stylesheet" type="text/css" href="dist/css/min/userRegLogin.min.css"/>
+		<!--引入图标字体-->
+		<link rel="stylesheet" type="text/css" href="lib/font-awesome/css/font-awesome.min.css"/>
+		<!--兼容低版本的浏览器-->
+		<!--[if lt IE 9]>
+	      <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	      <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+
+	<body>
+		<!--页头-->
+		<?php
+			include_once("./header.html");	
+		?>
+		
+		
+		<!--模板内容开始-->
+		<div class="container">
+			<h1>模板内容</h1>
+		</div>
+		
+		
+		
+		
+		<!--页脚-->
+		<?php
+			include_once("./footer.html");	
+		?>
+	</body>
+	<!--引入jQuery-->
+	<script src="lib/jquery/jquery-1.11.0.js"></script>
+	<!--引入bootstrap  js文件-->
+	<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+	<!--引入提示框的js文件-->
+	<script src="src/javascript/titlebox.js"></script>
+</html>
